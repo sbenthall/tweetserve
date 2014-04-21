5f5a2af32a5f82bb17f9e1563f97f62e9d8adff5
sb <sbenthall@gmail.com>
2014-04-21 11:44:00 -0700

    ok i guess this is a flask app now. going to try Google App Engine

---
+++ flaskapp/main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, nothing at this URL.', 404
